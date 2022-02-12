4693705ed839db555fb49c734fad4099daab8994
Wanjiru-Kamau <njirukmaureen@gmail.com>
2022-02-12 15:52:22 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var names = arrayOf("Maureen", "Rehema", "Yasin")
+    println(names[0])
+    var ages = arrayOf(19, 20, 21)
+    println(ages[0])
+    var numbers = arrayOf("0711413263", "0765432143", "0754376543")
+    println(numbers[2])
+    var weight = arrayOf(64, 65, 61)
+    println(weight[1])
+    var countries =arrayOf("kenya", "uganda", "sudan")
+    println(countries[2])
+}
+
+
+
